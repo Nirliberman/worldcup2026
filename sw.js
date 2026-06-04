4d19fef77fa4b5e81e2f4956a6bcea5f1ec0b90d
DESKTOP-E34N9SC\Nir <nirliberman9@gmail.com>
2026-06-04 10:38:30 +0300

    World Cup 2026 app - Hebrew schedule with channels and Shabbat times

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'mondial2026-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;700;900&family=Bebas+Neue&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
